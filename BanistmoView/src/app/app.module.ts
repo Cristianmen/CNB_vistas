556cda06ca271065fd7705a12c60d20b3ffb9af4
DIR\cristian.c.meneses <cristian.c.meneses@accenture.com>
2017-12-05 10:52:06 -0500

    componentes

--- BanistmoView/src/app/app.module.ts
+++ BanistmoView/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuAdminComponent } from './menu-admin/menu-admin.component';
+import { MenuOperadorComponent } from './menu-operador/menu-operador.component';
+import { ErrorComponent } from './error/error.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuAdminComponent,
+    MenuOperadorComponent,
+    ErrorComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
---
+++ BanistmoView/src/app/menu-operador/menu-operador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-operador',
+  templateUrl: './menu-operador.component.html',
+  styleUrls: ['./menu-operador.component.scss']
+})
+export class MenuOperadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
