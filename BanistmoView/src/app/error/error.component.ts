a4718f04e2505ec49da7f055ac137ae00ca1e88a
osoriojuan77 <34067019+osoriojuan77@users.noreply.github.com>
2017-12-06 14:23:22 -0500

    Cambios gnerales

--- BanistmoView/src/app/error/error.component.ts
+++ BanistmoView/src/app/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router/src/router';
 
 @Component({
   selector: 'app-error',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+
+   }
+
+
+
 
   ngOnInit() {
   }
