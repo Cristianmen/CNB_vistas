7a6c631c2abf5b1716ec062deb68f55e8b0589ba
saagudelo1 <34067017+saagudelo1@users.noreply.github.com>
2017-12-07 13:01:22 -0500

    Revert "Feature/sp1 hu001 05 12 2017"

--- BanistmoView/src/app/app.module.ts
+++ BanistmoView/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,16 +10,13 @@ import { ErrorComponent } from './error/error.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
 import { RouterModule, Routes } from '@angular/router';
-import { LogoutComponent } from './logout/logout.component';
-import { CabeceraComponent } from './cabecera/cabecera.component';
 
 const routes: Routes = [
   { path: 'Administrador', component: MenuAdminComponent },
   { path: 'Operario', component: MenuOperadorComponent },
   { path: 'Login', component: LoginComponent },
+  { path: 'Error', component: ErrorComponent },
   { path: 'Contacto', component: ContactoComponent },
-  { path: 'error', component: ErrorComponent },
-  { path: 'logout', component: LogoutComponent },
   { path: '**', redirectTo: 'Login', pathMatch: 'full' }
 ];
 @NgModule({
@@ -29,17 +26,10 @@ const routes: Routes = [
     MenuAdminComponent,
     MenuOperadorComponent,
     ErrorComponent,
-    ContactoComponent,
-    LogoutComponent,
-    CabeceraComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-      routes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- BanistmoView/src/app/error/error.component.ts
+++ BanistmoView/src/app/error/error.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router/src/router';
 
 @Component({
   selector: 'app-error',
@@ -8,13 +7,7 @@ import { Router } from '@angular/router/src/router';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() {
-
-
-   }
-
-
-
+  constructor() { }
 
   ngOnInit() {
   }
