da990e1fa1e8282587fd5c1be96f366b2cb8bacd
osoriojuan77 <34067019+osoriojuan77@users.noreply.github.com>
2017-12-07 13:33:17 -0500

    Respaldo

--- BanistmoView/src/app/cabecera/cabecera.component.ts
+++ BanistmoView/src/app/cabecera/cabecera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-cabecera',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceraComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router, ) { }
+  loged: Boolean = false;
   ngOnInit() {
+    if (this.router.url.toUpperCase().includes("Operario".toUpperCase()) || this.router.url.toUpperCase().includes("Administrador".toUpperCase()))
+      this.loged = true;
+    else
+      this.loged = false;
+
+
   }
 
 }
